176ac803ac322b18d1077d77b14920828ab139af
UnicornsExist <40534710+UnicornsExist@users.noreply.github.com>
2018-06-26 21:01:44 +0300

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,16 +1,31 @@
 # This script contains all algorithms I use in bot
-
-def referral_link_check(user_id, link):
-	# (ВАЖНО!)
-	# Здесь я должен проверить человека, нажавшего на /start на
-	# его наличие в базе данных бота для того, чтобы один человек
-	# не мог дважды стать чьим-то рефералом
-
-	if len(link.split(' ')) == 2:    # Если линка выглядит следующим образом : /start 12345, где 12345 - ID пригласившего человека
-		
+import pymongo
+from pymongo import MongoClient
 
 
+client = MongoClient()
+db = client.main_db
+
+users_coll = db.users_coll
 
 
+def insert_user(message):     # Добавляет юзера в БД
+	username = message.chat.username
+	user_id = message.chat.id
+	balance = 0
 
+	if len(message.text.split()) == 2: # Проверяет, перешёл ли юзер по реферральной ссылке
+		referer = message.text.split()[1]
+	else:
+		referer = ""
 
+	users_coll.insert({'username':username,
+		               'telegram_id':user_id,
+		               'balance':balance,
+		               'referer': referer})
+	
+
+def start(message):        # Проверяет, еcть ли юзер в БД
+	user_id = message.chat.id
+	if users_coll.find_one({'telegram_id':user_id}) == None:
+		insert_user(message)  # Добавляем юзера в БД
